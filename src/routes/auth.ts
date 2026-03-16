90d7814d747da36d15997ccc592dac30ac0bda09
veignma22 <145013759+veignma22@users.noreply.github.com>
2026-03-16 18:48:58 +0100

    Delete src directory

--- src/routes/auth.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { register, login } from '../authController';
-
-const router = Router();
-
-router.post('/register', register);
-router.post('/login', login);
-
-export default router;
